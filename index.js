2f0cb42d339fbc72b2a788b1e7bbdff02c6d5de2
Yolanda Diaz <yolicroni@hotmail.com>
2022-12-11 18:46:07 +0100

    funciona borrado completo

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 
 let buttonCounter = 0
 const buttonElement = document.querySelector('#addtrans');
-//const buttonRemover = document.querySelector('#remover${buttonCounter}');
+const buttonRemover = document.querySelector(`#remover${buttonCounter}`);
 let expendListElement = document.querySelector('#register');
 let incomeElement = document.querySelector('#income');
 let expensesElement = document.querySelector('#expenses');
 let totalElement = document.querySelector('#total');
+let listRegister = [];
 let positive = 0;
 let negative = 0;
 let total = 0;
 
 function saveRegister(expense) {
-  buttonCounter = buttonCounter + 1;
   //crear la info
   let expenseElement = ` ${expense.concept} ${expense.quantity} 
-  <button id= "remover${buttonCounter}">❌</button>  `;
+  <button class="buttonRemove" onclick="removeClicked(${buttonCounter})">❌</button> `;
   //crear la tag
   let expenseTag = document.createElement("p");
+  //añadir id al párrafo
+  expenseTag.setAttribute("id", `${buttonCounter}`);
   //meter info en tag
   expenseTag.innerHTML = expenseElement;
   //metar tag en el nodo de html
@@ -32,13 +34,40 @@ function saveEntry(expense) {
     negative = negative + cant;
     expensesElement.innerHTML = (negative)
   };
-  total = positive + negative + "€";
+  total = positive + negative + " €";
   totalElement.innerHTML = (total)
 };
+
+function deleteEntry(x) {
+//buscar la cant borrada
+//let prueba = listRegister.id === x
+//si es + restarla de income, si es negativa, sumarla a expenses
+  for (let index = 0; index < listRegister.length; index ++) {
+    if (listRegister[index].id === x) {
+      let cantRem = listRegister[index].quantity;
+      if (cantRem >= 0) {
+        positive = positive - cantRem;
+        incomeElement.innerHTML = (positive)
+      } else if (cantRem < 0) {
+        negative = negative - cantRem;
+        expensesElement.innerHTML = (negative)
+      };
+      total = positive + negative + " €";
+      totalElement.innerHTML = (total)
+    }
+  }
+};
+
+function removeClicked(x) {
+  const prrev = document.getElementById(x);
+  prrev.remove();
+  deleteEntry(x);
+}; 
 
 // añadir listener al evento click del botón
 buttonElement.addEventListener("click", (event) => {
   event.preventDefault();
+  buttonCounter ++;
   const inputConcept = document.querySelector('#concept');
   const inputQuantity = document.querySelector("#quantity");
   // guardamos el valor en localStorage
@@ -46,8 +75,10 @@ buttonElement.addEventListener("click", (event) => {
   //localStorage.setItem("savedCant", inputQuantity.value);
   let expense = {
     concept: inputConcept.value,
-    quantity: inputQuantity.value
+    quantity: parseFloat(inputQuantity.value),
+    id: buttonCounter,
   };
+  listRegister.push (expense);
   // ejecutar la función que guarda los datos en el historial
   saveRegister(expense);
 
@@ -59,8 +90,4 @@ buttonElement.addEventListener("click", (event) => {
   inputQuantity.value = "";
 });
 
-// Añadir listener al evento click del remover
-//buttonRemover.addEventListener("click", (event) => {
-//  event.preventDefault();
 
-//})
